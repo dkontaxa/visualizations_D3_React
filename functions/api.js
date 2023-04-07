909fee4fc781263cc7ffc2e4f4f44e1a703a071b
dkontaxa <deliakontaxakis@gmail.com>
2023-04-07 12:23:28 -0700

    changes package.json with tutorial

---
+++ functions/api.js
@@ -0,0 +1,9 @@
+exports.handler = async function(event, context) {
+    const url = 'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  };
