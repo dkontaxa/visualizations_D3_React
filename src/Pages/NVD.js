d1771771af1b8b7f3579a93081706ba7422dfc6d
dkontaxa <deliakontaxakis@gmail.com>
2023-05-11 15:16:27 -0700

    fixes apiNVD inside NVD component

--- src/Pages/NVD.js
+++ src/Pages/NVD.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from "react";
 // import AreaChart from "../src/Components/AreaChart";
 import "./App.css";
 
-const NVD = ({ loading, dataNVDTotal }) => {
+const NVD = () => {
   const [result, setResult] = useState([]);
 
-  useEffect(() => {
-    fetch("/.netlify/functions/apiNVD")
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setResult(jsonResponse);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-  console.log(result);
+  // useEffect(() => {
+  //   fetch("/.netlify/functions/apiNVD")
+  //     .then((response) => response.json())
+  //     .then((jsonResponse) => {
+  //       setResult(jsonResponse);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
+  // console.log(result);
 
   return (
     <div className="App">
