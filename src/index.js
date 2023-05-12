96547d7a17471a0b725576e5ded8438b841b16b2
dkontaxa <deliakontaxakis@gmail.com>
2023-05-11 22:49:34 -0700

    import App to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import App from "./App";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
