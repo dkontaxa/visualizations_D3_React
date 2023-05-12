04867361069c3e2bc49d9503e8849df12621aadb
dkontaxa <deliakontaxakis@gmail.com>
2023-05-11 22:39:56 -0700

    changes to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,13 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CISA from "./Pages/CISA";
 import NVD from "./Pages/NVD";
 
-const AppRoutes = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<CISA />} />
-        <Route path="/NVD" element={<NVD />} />
-      </Routes>
-    </Router>
-  );
-};
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<CISA />} />
+    <Route path="/NVD" element={<NVD />} />
+  </Routes>
+);
 
 export default AppRoutes;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
