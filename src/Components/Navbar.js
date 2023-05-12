7550b309547ada39e44937c12a8580cc1cd8b151
dkontaxa <deliakontaxakis@gmail.com>
2023-05-11 20:52:54 -0700

    add imports

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Link from "react-router-dom";
 import * as d3 from "d3";
 import OutlinedCard from "./Card";
 import CISA from "../Pages/CISA";
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { Router, Routes } from "react-router-dom";
 
 const Routes = () => {
   return (
