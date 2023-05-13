44b79dfef53bcbd77320b6f9f76f440093e006f5
dkontaxa <deliakontaxakis@gmail.com>
2023-05-12 23:31:16 -0700

    css changes

--- src/Pages/CISA.js
+++ src/Pages/CISA.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AreaChart from "../Components/AreaChart";
-const CISA = ({ loading, result }) => {
+const CISA = ({ loading, resultCISA }) => {
   return (
     <div>
       <h2>CISA component</h2>
@@ -8,7 +8,7 @@ const CISA = ({ loading, result }) => {
         <p>Loading...</p>
       ) : (
         <AreaChart
-          data={result}
+          data={resultCISA}
           width={500}
           height={200}
           margin={{ top: 20, right: 20, bottom: 50, left: 50 }}
