bb409c97eb171e9ddaa8cb41ed66e589d14a51b6
dkontaxa <deliakontaxakis@gmail.com>
2023-05-12 22:41:32 -0700

    add changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import React, { useState } from "react";
-import "./App.css";
-import AppRoutes from "./Routes";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import * as d3 from "d3";
+import CISA from "./Pages/CISA";
+import NVD from "./Pages/NVD";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
   const [dataCISATotal, setDataCISATotal] = useState([]);
+  const [dataNVDTotal, setDataNVDTotal] = useState([]);
+  const [resultCISA, setResultCISA] = useState([]);
+  const [resultNVD, setResultNVD] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/.netlify/functions/api")
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setDataCISATotal(jsonResponse.count);
+        const dataJson = jsonResponse.vulnerabilities.map((vulnerability) => ({
+          x: vulnerability.cveID.substr(4, 4),
+          y: 1,
+        }));
+        const sumYByYear = d3.rollup(
+          dataJson,
+          (v) => d3.sum(v, (d) => d.y),
+          (d) => d.x
+        );
+        const dataCISA = Array.from(sumYByYear, ([x, y]) => ({ x, y }));
+        // sort dataCISA by year in ascending order
+        dataCISA.sort((a, b) => a.x - b.x);
+        setResultCISA(dataCISA);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    fetch("/.netlify/functions/api/nvd")
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setDataNVDTotal(jsonResponse.totalCount);
+        setResultNVD(jsonResponse.result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <Navbar dataCISATotal={dataCISATotal} />
-      <AppRoutes setDataCISATotal={setDataCISATotal} />
+      <Navbar dataCISATotal={dataCISATotal} dataNVDTotal={dataNVDTotal} />
+      <Routes>
+        <Route
+          path="/"
+          element={<CISA result={resultCISA} loading={loading} />}
+        />
+        <Route
+          path="/NVD"
+          element={<NVD result={resultNVD} loading={loading} />}
+        />
+      </Routes>
     </div>
   );
 };
--- src/Routes.js
+++ src/Routes.js
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import * as d3 from "d3";
-import CISA from "./Pages/CISA";
-import NVD from "./Pages/NVD";
+// import React, { useState, useEffect } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import * as d3 from "d3";
+// import CISA from "./Pages/CISA";
+// import NVD from "./Pages/NVD";
 
-const AppRoutes = ({ setDataCISATotal }) => {
-  const [result, setResult] = useState([]);
-  const [loading, setLoading] = useState(true);
+// const AppRoutes = ({ setDataCISATotal }) => {
+//   const [result, setResult] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("/.netlify/functions/api")
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setDataCISATotal(jsonResponse.count);
-        const dataJson = jsonResponse.vulnerabilities.map((vulnerability) => ({
-          x: vulnerability.cveID.substr(4, 4),
-          y: 1,
-        }));
-        const sumYByYear = d3.rollup(
-          dataJson,
-          (v) => d3.sum(v, (d) => d.y),
-          (d) => d.x
-        );
-        const dataCISA = Array.from(sumYByYear, ([x, y]) => ({ x, y }));
-        // sort dataCISA by year in ascending order
-        dataCISA.sort((a, b) => a.x - b.x);
-        setResult(dataCISA);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [setDataCISATotal]);
+//   useEffect(() => {
+//     fetch("/.netlify/functions/api")
+//       .then((response) => response.json())
+//       .then((jsonResponse) => {
+//         setDataCISATotal(jsonResponse.count);
+//         const dataJson = jsonResponse.vulnerabilities.map((vulnerability) => ({
+//           x: vulnerability.cveID.substr(4, 4),
+//           y: 1,
+//         }));
+//         const sumYByYear = d3.rollup(
+//           dataJson,
+//           (v) => d3.sum(v, (d) => d.y),
+//           (d) => d.x
+//         );
+//         const dataCISA = Array.from(sumYByYear, ([x, y]) => ({ x, y }));
+//         // sort dataCISA by year in ascending order
+//         dataCISA.sort((a, b) => a.x - b.x);
+//         setResult(dataCISA);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   }, [setDataCISATotal]);
 
-  return (
-    <Routes>
-      <Route path="/" element={<CISA result={result} loading={loading} />} />
-      <Route path="/NVD" element={<NVD />} />
-    </Routes>
-  );
-};
+//   return (
+//     <Routes>
+//       <Route path="/" element={<CISA result={result} loading={loading} />} />
+//       <Route path="/NVD" element={<NVD />} />
+//     </Routes>
+//   );
+// };
 
-export default AppRoutes;
+// export default AppRoutes;
